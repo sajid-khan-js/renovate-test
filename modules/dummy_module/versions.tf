1027f8ea1be1f71d5dca2d32b260fdbab0e8b40e
sajid-khan-js <sajid.khan1@sainsburys.co.uk>
2021-08-05 20:47:21 +0100

    test

--- modules/dummy_module/versions.tf
+++ modules/dummy_module/versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "~> 0.14.8"
+  required_version = ">= 0.14.8"
 
   required_providers {
     null = {
       source  = "hashicorp/null"
-      version = "~> 3.0.0"
+      version = ">= 3.0.0"
     }
   }
 }

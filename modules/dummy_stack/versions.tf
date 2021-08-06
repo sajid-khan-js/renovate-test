dd6732d80d626f015c3120235e8a37dae6bb4446
sajid-khan-js <sajid.khan1@sainsburys.co.uk>
2021-08-06 13:47:35 +0100

    feat(renovate): add groups for packagerules

--- modules/dummy_stack/versions.tf
+++ modules/dummy_stack/versions.tf
@@ -1,10 +1,14 @@
 terraform {
-  required_version = "~> 0.14.4"
+  required_version = "~> 1.0.0"
 
   required_providers {
     random = {
       source  = "hashicorp/random"
-      version = "~> 2.3"
+      version = "~> 3.1.0"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.1.0"
     }
   }
 }
